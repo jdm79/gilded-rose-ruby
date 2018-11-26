f49d354d1396362923298774ba8cff7c97da5e2b
jdm79 <james.malvern@gmail.com>
2018-11-26 15:56:26 +0000

    refactor: standard hand of rag quality updater

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -4,10 +4,15 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality( qualityUpdater = 1)
     @items.each do |item|
-
-      
+      case item.name
+        when 'Sulfuras, Hand of Ragnaros' 
+          qualityUpdater = 0
+      end
+      item.sell_in = item.sell_in - 1
+      item.quality = item.quality - qualityUpdater
+    end   
   end
 end
 
