d94464a3f3da7a48b629aaf8510e7313df5df20b
James Sutherland <jrsutherland78@googlemail.com>
2018-11-26 15:15:07 +0000

    test: quaility standard item, aged brie

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -15,37 +15,54 @@ describe GildedRose do
   #   Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
   # ]
 
-
   describe "#update_quality" do
-    it "reduces sellin date daily" do
-      items = [Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq(9)
+
+    describe "sell in date" do
+
+      it "reduces sellin date daily" do
+        items = [Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq(9)
+      end
+      it "reduces sellin date of aged brie by 1 daily" do
+        items = [Item.new(name="Aged Brie", sell_in=2, quality=0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq(1)
+      end
+      it "keeps sellin date of hand rag the same" do
+        items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq(0)
+      end
+      it "keeps sellin date of hand rag the same (-1)" do
+        items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq(-1)
+      end
+      it "reduces sellin date of backstage pass by 1 daily" do
+        items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq(14)
+      end
+      it "reduces conjured sellin date by 1 daily" do
+        items = [Item.new(name="Conjured Mana Cake", sell_in=3, quality=6)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq(2)
+      end
+
     end
-    it "reduces sellin date of aged brie by 1 daily" do
-      items = [Item.new(name="Aged Brie", sell_in=2, quality=0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq(1)
-    end
-    it "keeps sellin date of hand rag the same" do
-      items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq(0)
-    end
-    it "keeps sellin date of hand rag the same (-1)" do
-      items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq(-1)
-    end
-    it "reduces sellin date of backstage pass by 1 daily" do
-      items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq(14)
-    end
-    it "reduces conjured sellin date by 1 daily" do
-      items = [Item.new(name="Conjured Mana Cake", sell_in=3, quality=6)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq(2)
+
+    describe "quality reduction" do
+      it "standard item reduces quality by 1 daily" do
+        items = [Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq(19)
+      end
+      it "reduces sellin date of aged brie by 1 daily" do
+        items = [Item.new(name="Aged Brie", sell_in=2, quality=0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq(1)
+      end
     end
   end
 end
